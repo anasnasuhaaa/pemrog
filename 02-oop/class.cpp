f4debf75febb64e935f8a38507cf96c211e2be1a
Anas Nasuha <infoanaspedia@gmail.com>
2026-06-29 15:24:50 +0700

    add class to 02-oop

---
+++ 02-oop/class.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class myClass
+{
+private:
+  int a, b;
+
+public:
+  void set(int p1, int p2)
+  {
+    a = p1;
+    b = p2;
+  }
+  void setA(int p)
+  {
+    a = p;
+  }
+  void setB(int p)
+  {
+    b = p;
+  }
+  int sum()
+  {
+    return a + b;
+  }
+  int min()
+  {
+    return a - b;
+  }
+};
+
+int main()
+{
+  myClass myObj;
+  myObj.set(10, 10);
+
+  cout << myObj.sum() << endl;
+  cout << myObj.min() << endl;
+
+  myObj.setA(50);
+  myObj.setB(40);
+
+  cout << myObj.sum() << endl;
+  cout << myObj.min() << endl;
+
+  return 0;
+}
